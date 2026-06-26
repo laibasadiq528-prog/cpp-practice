01cde21b186fcf9e8c4f525802d100efd5fc2632
layba sadiq <laibasadiq528@gmail.com>
2026-06-26 15:15:26 +0530

    Add files via upload

---
+++ input_output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+		string name;
+		int age;
+		
+		cout << "Enter your name:  ";
+		cin >> name;
+		
+		cout << "Enter your age:  ";
+		cin >> age;
+		
+		cout << "\n Your Name: "<< name << endl;
+		cout << "\n Your Age: "<< age << endl;
+		
+	
+	
+	return 0;
+}
