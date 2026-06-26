82f3105d2e86e5c72d018a70fbceaab1417f6e9d
layba sadiq <laibasadiq528@gmail.com>
2026-06-26 13:06:32 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//variable declaration
+	int age = 20;
+	float height = 5.2;
+	char grade = 'A';
+	string name = "laiba";
+	bool isStudent = true;
+	
+	//dsplay output
+	cout << "Name:" << name << endl;
+	cout << "Age:" << age << endl;
+	cout << "Height:" << height << endl;
+	cout << "Grade:" << grade << endl;
+	cout << "Student:" << isStudent << endl;
+	
+	
+	
+	return 0;
+}
